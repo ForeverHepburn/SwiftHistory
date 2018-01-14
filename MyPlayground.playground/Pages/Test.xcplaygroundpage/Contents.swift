b280cd3b07c869236795750f9f3377f91405256a
lwl <fanyu289@163.com>
2018-01-14 23:04:51 +0800

    Swift 4.1

--- MyPlayground.playground/Pages/Test.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Test.xcplaygroundpage/Contents.swift
@@ -47,7 +47,8 @@ emptyDict["key1"] = "value1"
 emptyDict
 
 
-var optionalString: String? = "Hello"
+var optionalString: String?
+    = "Hello"
 print(optionalString == nil)
 var greeting = "Hello!"
 if let name = optionalString {
@@ -55,4 +56,133 @@ if let name = optionalString {
 } else {
     greeting = "Hello, boy"
 }
+
+var numbers = [20,19,7,12]
+numbers.map ({ (number: Int) -> Int in
+    let result = 3*num
+    return result
+})
+
+print(numbers)
+
+let result = numbers.map({number in 3*number})
+print(result)
+
+let sorrtedNumbers = numbers.sorted{$0>$1}
+print(sorrtedNumbers)
+
+
+class testClass {
+    let num = 0
+    var name: String
+    var numberOfSides: Int = 0
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func addOne(number: Int) -> Int {
+        return number + 1
+    }
+}
+
+var test = testClass.init(name: "name")
+test.addOne(number: 1)
+print(test.addOne(number: 2))
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class Square: NamedShape {
+    var sideLength: Double
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)"
+    }
+}
+
+let square = Square(sideLength: 10, name: "square")
+square.area()
+square.simpleDescription()
+
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+}
+
+var triangle = EquilateralTriangle.init(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.perimeter)
+
+print(triangle.sideLength)
+
+
+enum Suit {
+    case spades, hearts, diamonds, clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+}
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+
+
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "a"
+    func adjust() {
+        simpleDescription += "---"
+    }
+    
+}
+
+
 
---
+++ MyPlayground.playground/Pages/swift4.1_基本运算符.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+var red, green, blue: Double
+red = 0.10
+
+print(red, separator: "1", terminator: "222")
+
+var minValue = UInt8.min
+var maxValue = UInt8.max
+
+var space = "111"
+var intValue = 1
+var floatValue = 1.0
+
+print(space)
+
+var boolValue = true
+boolValue = false
+
+let i: Int? = 1
+if let value = i {
+    print(1111)
+}
+
+
+let httpError = (404, "Not Found")
+print(httpError)
+httpError.0
+httpError.1
+
